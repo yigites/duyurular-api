aca2617333fb4732b01ab9af905f07e26a38223f
Yiğit <yigiterensaklarr@gmail.com>
2025-12-17 14:26:41 +0300

    bug fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const PUBLIC_BASE_URL = "https://KULLANICIADI.github.io/multi-duyurular-api";
+export const PUBLIC_BASE_URL = "https://yigites.github.io/duyurular-api";
 export const DATA_DIR = "data";
 export const PUBLIC_DIR = "public";
